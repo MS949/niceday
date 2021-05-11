5cbbc535ae9f5c5efb21297c69aece9262f21534
Hanyul Park <yoll0765@gmail.com>
2021-05-12 04:06:33 +0900

    feat : main_fragment add
    Make activity_main fragment view
    Add MainActivity for main_fragment
    Add MainFragment, main_fragment for mainView
    Fix crash where Actionbar

--- app/src/main/java/com/ms949/niceday/MainActivity.java
+++ app/src/main/java/com/ms949/niceday/MainActivity.java
@@ -1,15 +1,45 @@
 package com.ms949.niceday;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.ClipData;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.Window;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MainFragment mainFragment = new MainFragment();
+    private FragmentManager fragmentManager = getSupportFragmentManager();
+    private FragmentTransaction transaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
+        transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.frameLayout, mainFragment).commitAllowingStateLoss();
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new ItemSelectedListener());
+    }
+
+    class ItemSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            transaction = fragmentManager.beginTransaction();
+            switch (item.getItemId()) {
+                case R.id.page_2:
+                    transaction.replace(R.id.frameLayout, mainFragment).commitAllowingStateLoss();
+                    return true;
+            }
+            return false;
+        }
     }
 }
