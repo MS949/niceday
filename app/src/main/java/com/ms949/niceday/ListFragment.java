c493bc5e773ecfea843321db748a8687adf18040
Hanyul Park <yoll0765@gmail.com>
2021-05-15 06:09:28 +0900

    feat : Add success_activity
    Add SuccessActivity, success_activity for successView
    Add setOnClickListener to ListFragment

--- app/src/main/java/com/ms949/niceday/ListFragment.java
+++ app/src/main/java/com/ms949/niceday/ListFragment.java
@@ -1,9 +1,11 @@
 package com.ms949.niceday;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,6 +15,18 @@ public class ListFragment extends Fragment {
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.list_fragment, container, false);
+        View view = inflater.inflate(R.layout.list_fragment, container, false);
+
+        Button btn = view.findViewById(R.id.list_check_btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), SuccessActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        return view;
     }
 }
