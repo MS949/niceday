f8bb56a84818b4c2af0c88e953f57adb4bbf5839
Hanyul Park <yoll0765@gmail.com>
2021-05-14 22:33:32 +0900

    feat : setting_fragment add
    Rename activity_main to main_activity
    Add SettingFragment,setting_fragment for settingView
    Add FrameLayout weight to main_activity

--- app/src/main/java/com/ms949/niceday/MainActivity.java
+++ app/src/main/java/com/ms949/niceday/MainActivity.java
@@ -1,30 +1,30 @@
 package com.ms949.niceday;
 
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.Window;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.content.ClipData;
-import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.Window;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private MainFragment mainFragment = new MainFragment();
-    private FragmentManager fragmentManager = getSupportFragmentManager();
-    private FragmentTransaction transaction;
+    MainFragment mainFragment = new MainFragment();
+    SettingFragment settingFragment = new SettingFragment();
+    FragmentManager fragmentManager = getSupportFragmentManager();
+    FragmentTransaction transaction = fragmentManager.beginTransaction();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_activity);
 
-        transaction = fragmentManager.beginTransaction();
         transaction.replace(R.id.frameLayout, mainFragment).commitAllowingStateLoss();
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_view);
         bottomNavigationView.setOnNavigationItemSelectedListener(new ItemSelectedListener());
@@ -35,11 +35,26 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             transaction = fragmentManager.beginTransaction();
             switch (item.getItemId()) {
+                case R.id.page_1:
+                    transaction.replace(R.id.frameLayout, settingFragment).commitAllowingStateLoss();
+                    return true;
                 case R.id.page_2:
                     transaction.replace(R.id.frameLayout, mainFragment).commitAllowingStateLoss();
+                    return true;
+                case R.id.page_3:
+//                    transaction.replace(R.id.frameLayout, mainFragment).commitAllowingStateLoss();
+                    showToast("page_3");
+                    return true;
+                case R.id.page_4:
+//                    transaction.replace(R.id.frameLayout, mainFragment).commitAllowingStateLoss();
+                    showToast("page_4");
                     return true;
             }
             return false;
         }
+    }
+    void showToast(String msg) {
+        Toast toast = Toast.makeText(this.getApplicationContext(), msg, Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
---
+++ app/src/main/java/com/ms949/niceday/SettingFragment.java
@@ -0,0 +1,54 @@
+package com.ms949.niceday;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class SettingFragment extends Fragment {
+
+    Spinner spinner;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.setting_fragment, container, false);
+        spinner = view.findViewById(R.id.spinner);
+        spinnerSelect();
+
+        return view;
+    }
+
+    void spinnerSelect() {
+        String[] spinnerArray = "일정시간마다 경고,핸드폰 제한,아무것도 하지 않음".split(",");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, spinnerArray);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                showToast(spinnerArray[position]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                showToast("null");
+            }
+        });
+    }
+
+    void showToast(String msg) {
+        Toast toast = Toast.makeText(getActivity(), msg, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+}
